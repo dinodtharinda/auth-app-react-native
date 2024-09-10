97ec706b18605a4de19c21f0df3c9073af67d671
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-09-10 18:12:48 +0530

    error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { login } from '../util/auth';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 
-function LoginScreen() {  
+function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function signinHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+
+    try {
+      await login(email, password);
+    } catch (email) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in. Please check your credenctials or try again later!"
+      );
+    }
+
     setIsAuthenticating(false);
   }
 
-  if(isAuthenticating){
-    return <LoadingOverlay message="Creating user..."/>
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
   }
 
   return <AuthContent isLogin onAuthenticate={signinHandler} />;
