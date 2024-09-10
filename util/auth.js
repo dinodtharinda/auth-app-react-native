72d584e01856571ffc82489884ef3ad690a601cb
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-09-10 18:02:38 +0530

    add login function

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
+import { API_KEY } from "../api_keys";
 
+export async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, { email: email, password: password });
+
+  console.log(response);
+}
 
 export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  const response = authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  const response = await authenticate("signInWithPassword", email, password);
 }
